cb3d833d8bac55556e5e98a99f5c8f892f5ca1d7
Kivi Corn <kivi@Kivis-MBP.lan>
2023-07-11 20:44:59 +0100

    STYLE: fix IOS notch issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,9 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-4">
-      <div>
-        <h1 className='font-mono text-3xl'>Turn Masterss</h1>
-        <img className="w-8 h-8 rounded absolute right-5 top-5" src="https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="Default avatar" />
+      <div className='relative w-full text-center'>
+        <h1 className='font-mono text-3xl'>Turn Masters</h1>
+        <img className="w-8 h-8 rounded absolute right-1 top-1" src="https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="Default avatar" />
       </div>
       <div className="z-10 w-full max-w-5xl items-stretch justify-between font-mono text-sm flex flex-col gap-4 ">
         <Link href="/single-player" className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100">
