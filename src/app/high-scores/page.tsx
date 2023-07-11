90f92a759414bfbb4d1b17f852a8f43cce89ea35
Kivi Corn <kivi@Kivis-MBP.lan>
2023-07-11 21:52:02 +0100

    STYLE: improve high scores page

--- src/app/high-scores/page.tsx
+++ src/app/high-scores/page.tsx
@@ -10,9 +10,9 @@ const HighScoresPage = () => {
             <path fillRule="evenodd" d="M7.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l2.293 2.293a1 1 0 010 1.414z" clipRule="evenodd"></path>
           </svg>
         </Link>
-        <h1 className="text-center text-lg">High Scores</h1>
+        <h1 className="text-center text-lg font-mono">High Scores</h1>
       </div>
-      <div className="shadow shadow-lg p-6 mt-8">
+      <div className="bg-white shadow-lg p-6 mt-8">
         <HighScores />
       </div>
     </div>
