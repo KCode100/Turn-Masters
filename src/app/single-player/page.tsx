f079e229f0eeda348eb264445d4c64810261e92b
Kivi Corn <kivi.webdev@gmail.com>
2023-07-11 10:40:30 +0100

    STYLE: animate result

--- src/app/single-player/page.tsx
+++ src/app/single-player/page.tsx
@@ -1,8 +1,10 @@
+import TimeGuessGame from "@/components/TimeGuessGame";
 import TowerGame from "@/components/TowerGame";
 import { ReactNode } from "react";
 
 const GAMES: ReactNode[] = [
   <TowerGame key="towerGame" />,
+  <TimeGuessGame key="timeGuessGame" />,
 ]
 
 const SinglePlayer = () => {
