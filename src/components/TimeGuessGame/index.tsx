cc8c412c4819675c45f5989fdaa5995dae402840
Kivi Corn <kivi@Kivis-MBP.lan>
2023-07-11 10:40:30 +0100

    STYLE: animate result

--- src/components/TimeGuessGame/index.tsx
+++ src/components/TimeGuessGame/index.tsx
@@ -1,54 +1,73 @@
-'use client'
-import React, { useState, useEffect } from 'react';
+/*
 
-const TimeGuessGame: React.FC = () => {
-  const [playPressed, setPlayPressed] = useState(false);
-  const [startTime, setStartTime] = useState(0);
-  const [stopTime, setStopTime] = useState(0);
-  const [elapsedTime, setElapsedTime] = useState(0);
+10 Second Stopwatch Game is a test of your body's internal clock.
+
+Objective:
+With your eyes closed, stop the clock as close to 10.000 seconds as possible without going over. (The Price is Right rules)
+
+Rules:
+Player must not be able to view or feel any time keeping source. (Basically, player should be blindfolded)
+
+*/
+
+'use client'
+import { useState, useEffect, FC } from 'react';
+import styles from './TimeGuessGame.module.scss'
+
+const TimeGuessGame: FC = () => {
+  const [time, setTime] = useState(0)
+  const [isPlaying, setIsPlaying] = useState(false)
+  const [gameComplete, setGameComplete] = useState(false)
+  const SECONDS_TO_GUESS = 10
 
   useEffect(() => {
-    let intervalId: NodeJS.Timeout;
-
-    if (playPressed) {
-      setStartTime(Date.now());
-      intervalId = setInterval(() => {
-        setElapsedTime(Date.now() - startTime);
+    let interval: NodeJS.Timeout | undefined;
+    if (isPlaying) {
+      interval = setInterval(() => {
+        setTime((prevTime) => prevTime + 10);
       }, 10);
+    } else if (!isPlaying) {
+      clearInterval(interval);
     }
 
-    return () => {
-      clearInterval(intervalId);
-    };
-  }, [playPressed]);
+    return () => clearInterval(interval);
+  }, [isPlaying]);
 
-  const handlePlayClick = () => {
-    setPlayPressed(true);
-  };
+  function handleStartTimer() {
+    setIsPlaying(true)
+  }
 
-  const handleStopClick = () => {
-    if (playPressed) {
-      setStopTime(Date.now());
-      setPlayPressed(false);
-    }
-  };
-
-  const actualTime = 10000; // 10 seconds in milliseconds
+  function handleEndTimer() {
+    setIsPlaying(false)
+    setGameComplete(true)
+  }
 
   return (
-    <div>
-      <button onClick={handlePlayClick} disabled={playPressed}>
-        Play
-      </button>
-      <button onClick={handleStopClick} disabled={!playPressed}>
-        Stop
-      </button>
-      {stopTime > 0 && (
-        <div>
-          <p>Actual Time: {actualTime / 1000} seconds</p>
-          <p>Your Time: {elapsedTime / 1000} seconds</p>
-        </div>
-      )}
+    <div className="min-h-screen flex flex-col items-center justify-center gap-32">
+      <div className={`text-center text-6xl font-mono ${gameComplete ? 'mb-0' : 'mb-12'}`}>
+        {isPlaying ? (
+          <span>##.##</span>
+        ) : (
+          <>
+            <span>{("0" + Math.floor((time / 1000) % 60)).slice(-2)}.</span>
+            <span>{("0" + ((time / 10) % 100)).slice(-2)}</span>
+          </>
+        )}
+        {gameComplete && (
+          <p className={`${styles.result} text-blue-500 block text-2xl text-center pt-4`}>{Math.abs((SECONDS_TO_GUESS - (time / 1000))).toFixed(2)} seconds out!</p>
+        )}
+      </div>
+      <div className='w-full'>
+        <button
+          onClick={ isPlaying ? handleEndTimer : handleStartTimer }
+          className="relative w-full m-auto p-0.5 max-w-xs block overflow-hidden text-lg font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-green-400 to-blue-600"
+          disabled={gameComplete}
+        >
+          <span className="relative block px-5 py-2.5 transition-all ease-in duration-75 bg-white rounded-md">
+            { (isPlaying || gameComplete) ? 'STOP' : 'START' }
+          </span>
+        </button>
+      </div>
     </div>
   );
 };
