387563aea0b6bfc21a84f33de5a17e46738ffd5a
Kivi Corn <kivi.webdev@gmail.com>
2023-07-14 02:01:26 +0100

    call and deleted buttons

--- src/components/PhoneNumberGame/index.tsx
+++ src/components/PhoneNumberGame/index.tsx
@@ -15,7 +15,7 @@ function generateRandomNumber():string {
 
 const PhoneNumberGame = () => {
   const [numbersClicked, setNumbersClicked] = useState<string>("")
-  const [numberToGuess, setNumberToGuess] = useState<string>(generateRandomNumber())
+  const [numberToGuess, setNumberToGuess] = useState<string>("")
 
   function handleClick(btnClicked: string) {
     setNumbersClicked(prev => prev += btnClicked)
@@ -26,25 +26,45 @@ const PhoneNumberGame = () => {
 
   function checkAnswer() {
     console.log(numbersClicked === numberToGuess);
+    console.log('alo');
+    
   }
-  
+
+  function handleDelete() {
+    const removeLastDigit = numbersClicked.slice(0, -1);
+    setNumbersClicked(removeLastDigit)
+    console.log('alo');
+  }
+
+  useEffect(() => {
+    setNumberToGuess(generateRandomNumber())
+  }, [])
 
   return ( 
     <div className="w-full">
+      <h1>{numberToGuess}</h1>
       <button onClick={checkAnswer}>Check</button>
       <div className="relative max-w-xs m-auto">
         <img className="block m-auto max-h-screen" src="/phone-keyboard.png" alt="phone keyboard" />
         <div className="grid grid-cols-3 gap-x-4 gap-y-2.5 place-content-start absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-38%] opacity-40 w-[60%] bg-green-200 m-auto" style={{aspectRatio: "200/320"}}>
-          <div className="m-auto bg-red-500 w-full rounded-full aspect-square" onClick={()=>handleClick("1")}></div>
-          <div className="m-auto bg-red-500 w-full rounded-full aspect-square" onClick={()=>handleClick("2")}></div>
-          <div className="m-auto bg-red-500 w-full rounded-full aspect-square" onClick={()=>handleClick("3")}></div>
-          <div className="m-auto bg-red-500 w-full rounded-full aspect-square" onClick={()=>handleClick("4")}></div>
-          <div className="m-auto bg-red-500 w-full rounded-full aspect-square" onClick={()=>handleClick("5")}></div>
-          <div className="m-auto bg-red-500 w-full rounded-full aspect-square" onClick={()=>handleClick("6")}></div>
-          <div className="m-auto bg-red-500 w-full rounded-full aspect-square" onClick={()=>handleClick("7")}></div>
-          <div className="m-auto bg-red-500 w-full rounded-full aspect-square" onClick={()=>handleClick("8")}></div>
-          <div className="m-auto bg-red-500 w-full rounded-full aspect-square" onClick={()=>handleClick("9")}></div>
-          <div className="m-auto bg-red-500 w-full rounded-full aspect-square" onClick={()=>handleClick("0")}></div>
+          <button className="m-auto bg-red-500 w-full rounded-full aspect-square" onClick={()=>handleClick("1")}></button>
+          <button className="m-auto bg-red-500 w-full rounded-full aspect-square" onClick={()=>handleClick("2")}></button>
+          <button className="m-auto bg-red-500 w-full rounded-full aspect-square" onClick={()=>handleClick("3")}></button>
+          <button className="m-auto bg-red-500 w-full rounded-full aspect-square" onClick={()=>handleClick("4")}></button>
+          <button className="m-auto bg-red-500 w-full rounded-full aspect-square" onClick={()=>handleClick("5")}></button>
+          <button className="m-auto bg-red-500 w-full rounded-full aspect-square" onClick={()=>handleClick("6")}></button>
+          <button className="m-auto bg-red-500 w-full rounded-full aspect-square" onClick={()=>handleClick("7")}></button>
+          <button className="m-auto bg-red-500 w-full rounded-full aspect-square" onClick={()=>handleClick("8")}></button>
+          <button className="m-auto bg-red-500 w-full rounded-full aspect-square" onClick={()=>handleClick("9")}></button>
+        </div>
+        <div className="absolute bottom-[30%] left-[50%] translate-x-[-50%] opacity-40 w-[70%] bg-green-200 m-auto">
+          <button className="block m-auto bg-red-500 w-1/4 rounded-full aspect-square" onClick={()=>handleClick("0")}></button>
+        </div>
+        <div className="absolute bottom-[20%] left-[50%] translate-x-[-50%] opacity-40 w-[70%] bg-green-200 m-auto">
+          <button className="block m-auto bg-red-500 w-1/4 rounded-full aspect-square" onClick={checkAnswer}></button>
+        </div>
+        <div className="absolute bottom-[20%] left-[75%] translate-x-[-50%] opacity-40 w-[70%] bg-green-200 m-auto">
+          <button className="block m-auto bg-red-500 w-1/4 rounded-full aspect-square" onClick={handleDelete}></button>
         </div>
       </div>
     </div>
